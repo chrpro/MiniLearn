71b3fbf6e22c8c4a969ab77a3b0df1aab4ceab2a
chrpro <chrpro@chalmers.se>
2023-01-22 13:41:35 +0100

    clean some parts

--- MiniLearn-on-HAR/src/main.c
+++ MiniLearn-on-HAR/src/main.c
@@ -32,47 +32,32 @@
 # define numberOFiltersConv1 32
 
 
-
-static q7_t conv3_w_2[numberOFilters*CONV3_IN_CH*CONV3_KER_DIM*CONV3_KER_DIM] =  {0};
-static q7_t conv3_b_2[numberOFilters] = {0};
-static q7_t conv3_out[numberOFilters*CONV3_OUT_DIM*CONV3_OUT_DIM];
-// static q7_t pool3_out[numberOFilters*POOL3_OUT_DIM*POOL3_OUT_DIM];
-
-
-
-// static q7_t interface_out[INTERFACE_OUT];
-
-static q7_t fc1_out[INTERFACE_OUT];
-
-static q7_t y_out[INTERFACE_OUT];
-
+// netwo
 uint32_t network(q7_t* input);
 uint32_t network2(q7_t* input);
 
 
+// static buffers for the CMSISNN computations.
+static q7_t conv3_w_2[numberOFilters*CONV3_IN_CH*CONV3_KER_DIM*CONV3_KER_DIM] =  {0};
+static q7_t conv3_b_2[numberOFilters] = {0};
+static q7_t conv3_out[numberOFilters*CONV3_OUT_DIM*CONV3_OUT_DIM];
+static q7_t fc1_out[INTERFACE_OUT];
+static q7_t y_out[INTERFACE_OUT];
 static q7_t conv1_out[CONV1_OUT_CH*CONV1_OUT_DIM*CONV1_OUT_DIM];
-// static q7_t pool1_out[CONV1_OUT_CH*POOL1_OUT_DIM*POOL1_OUT_DIM];
 static q7_t conv2_out[CONV2_OUT_CH*CONV2_OUT_DIM*CONV2_OUT_DIM];
-// static q7_t pool2_out[CONV2_OUT_CH*POOL2_OUT_DIM*POOL2_OUT_DIM];
-
 static q7_t conv1_w[CONV1_WT_SHAPE] = CONV1_WT;
 static q7_t conv1_b[CONV1_BIAS_SHAPE] = CONV1_BIAS;
 static q7_t conv2_w[CONV2_WT_SHAPE] =  CONV2_WT;
 static q7_t conv2_b[CONV2_BIAS_SHAPE] = CONV2_BIAS;
 static q7_t conv3_w[CONV3_WT_SHAPE] =  CONV3_WT;
 static q7_t conv3_b[CONV3_BIAS_SHAPE] = CONV3_BIAS;
-
 q7_t fc1_w[INTERFACE_WT_SHAPE] = INTERFACE_WT;
 q7_t fc1_b[INTERFACE_BIAS_SHAPE] = INTERFACE_BIAS;
-
-
 static q15_t conv_buffer[MAX_CONV_BUFFER_SIZE];
 static q15_t fc_buffer[MAX_FC_BUFFER];
 
 
-
-
-
+// batch size for training
 static int batch = 4;
 
 
@@ -90,37 +75,6 @@ struct node
 typedef struct node N;
 
 
-void insert( int item, float itprio)
-{
-    // int item, itprio; 
-    new = (N *)malloc(sizeof(N));
-
-    new->info = item;
-    new->priority = itprio;
-    new->next = NULL;
-    if (start == NULL)
-    {
-        start = new;
-    }
-    else if (start != NULL && itprio < start->priority)
-    {
-        new->next = start;
-        start = new;
-    }
-    else
-    {
-        q = start;
-        while (q->next != NULL && q->next->priority < itprio)
-        {
-            q = q->next;
-        }
-        new->next = q->next;
-        q->next = new;
-    }
-}
-
-
-
 
 int main(void)
 {
@@ -131,8 +85,10 @@ int main(void)
 		"============================================================\n"
 	}; printf(chalmersBanner);  
 
+
     int filterSize =  CONV3_IN_CH*CONV3_KER_DIM*CONV3_KER_DIM;
 
+    // an example how to compute the norm of w's
     for(int j =0 ; j < CONV3_OUT_CH; j ++ ){
         float l1norm=0, l2norm=0;
         float maxNorm = 0;
@@ -147,150 +103,74 @@ int main(void)
         printf("filter: %d , l1 norm : %f, l2 norm : %f , max norm : %f \n", j ,l1norm,l2norm , maxNorm);
         insert(j, l2norm);
     }
-    
     // insert(j, ArithMean);
 
-
+    // initilize the srand to get random numbers with different seed
     struct timeval t1;
     gettimeofday(&t1, NULL);
     srand(t1.tv_usec * t1.tv_sec);
 
 
-
     uint32_t index = 0;
     int PoolOut = CONV2_OUT_DIM* CONV2_OUT_DIM*numberOFiltersConv2;
-    // int PoolOut = POOL2_OUT_DIM* POOL2_OUT_DIM*CONV2_OUT_CH;
     
-
+    // Learning parameters
     float rate = .0001;
     float momentum = .9;
     float decay = .00001;
-    // int correct = 0;
-// NODE_0_TOTAL_TRAIN_IMAGES
+    // training data structure
     data training_data;
     int sub_sample = 50;
     matrix q_X0 = make_matrix(sub_sample,PoolOut);
     matrix q_Y0 = make_matrix(sub_sample,CLASSES);
 
-    // float scale = 1 << CONV1_OUT_Q;
-
+    // get the results of the last layers
     for(int img_row=0; img_row < sub_sample; img_row++)
     {
         index = network(NODE_0_TRAIN_IMAGES[img_row]);
-
-
         for (int i = 0; i < PoolOut; i++){
             q_X0.data[ img_row * q_X0.cols + i] = (float) conv2_out[i] /  powf(2,CONV2_OUT_Q) ;        
         }
         q_Y0.data[ (img_row * q_Y0.cols) + NODE_0_TRAIN_LABELS[img_row] ] = 1;     
     }
-
     training_data.x= q_X0;
     training_data.y = q_Y0;
     
-
-    // int correct = 0;
-    // for(int img_i=0; img_i < TOTAL_TEST_IMAGES; img_i++)
-    // {
-    //     index = network(TEST_IMAGES[img_i]);
-    //     if((uint32_t)TEST_LABELS[img_i] == index)
-    //         correct++;
-        
-    // }
-    // float acc = (float)correct / TOTAL_TEST_IMAGES;
-    // printf("=> CMSIS accuracy: %f\n",acc);
-
-
-    // return 0;
-    // matrix X_test = make_matrix(TOTAL_TEST_IMAGES,PoolOut);
-    // matrix Y_test = make_matrix(TOTAL_TEST_IMAGES,CLASSES);
-
-    // for(int img_i=0; img_i < TOTAL_TEST_IMAGES; img_i++)
-    // {
-    //     index = network(TEST_IMAGES[img_i]);
-    //     if(TEST_LABELS[img_i] == (q7_t) index){
-    //         correct++;
-    
-    //     }
-    //     for (int i = 0; i < PoolOut; i++){
-    //         X_test.data[ img_i*X_test.cols + i] = (float) pool2_out[i] /  powf(2,CONV2_OUT_Q) ;
-    //     }
-    //     Y_test.data[ (img_i*Y_test.cols) + TEST_LABELS[img_i] ] = 1;      
-    // }
-    // printf("=> CMSIS accuracy: %f\n", (float)correct / TOTAL_TEST_IMAGES );
-
-    // data test;
-    // test.x=X_test;
-    // test.y=Y_test;
-
-    // printf("iterations = %d\n",iterations);
-    // int convout = CONV3_OUT_CH*CONV3_OUT_DIM*CONV3_OUT_DIM;
-
+    // define the netwrok for training
     static net active_learning = {0}; 
     active_learning.n = 4;
     active_learning.layers = calloc(4, sizeof(layer));   
-    
-    //filter size 32*3*3
-
-    // number of filters 16
-    // make it 8
-    // int numberOFilters = 12 ;// ->CONV3_OUT_CH
-
-    // active_learning.layers[0] = make_convolutional_layer(CONV3_IM_DIM, CONV3_IM_DIM, CONV3_IM_CH, CONV3_OUT_CH, CONV3_KER_DIM , CONV3_STRIDE);
-
     active_learning.layers[0] = make_convolutional_layer(CONV3_IN_DIM, CONV3_IN_DIM, numberOFiltersConv2, numberOFilters, CONV3_KER_DIM , CONV3_STRIDE);
     
 
+    // For this layer we just copy the values
     temp = start;
     for (int filter = 0; filter < numberOFilters ;filter ++)
     {
         if (temp != NULL)
-        // printf("%d norm =%f\n", temp->info, temp->priority);
         temp=temp->next;
-        
     }
-
-
-   
-
+    // For the next layer we dequantize the values from int -> float
     temp = start;
     for (int filter = 0 ; filter < numberOFilters; filter++){
         int startIndex = temp ->info;
         temp=temp->next;
         int i = 0;
         for (int index = startIndex* filterSize; index <  filterSize * (startIndex+1); index ++){
+            //Here is where the dequantization happens
             active_learning.layers[0].w.data[filter * filterSize + i] = (float) (conv3_w[index]  ) /  powf(2, CONV3_WEIGHT_Q); 
-            // printf(" i=%d ,", filter * filterSize + i);
             i++;
         }
-            // printf("\n----\n");
     }
-
     temp = start;
     for (int index = 0; index < numberOFilters; index++)
     {
         int filter_bias = temp ->info;
-        // printf("Index\n")
         temp=temp->next;
         active_learning.layers[0].b.data[index] = (float)  conv3_b[filter_bias] /  powf(2, CONV3_BIAS_Q);
 
     }
 
-    // temp = start;
-
-    // for (int index = 0; index < CONV3_WT_SHAPE; index++)
-
-    // for (int index = 0; index < filterSize * numberOFilters; index++)
-    // {
-    //     active_learning.layers[0].w.data[index] = (float) (conv3_w[index]  ) /  powf(2, CONV3_WEIGHT_Q); 
-    //     // printf("%f, ",active_learning.layers[4].w.data[index]);
-    // }
-
-    // for (int index = 0; index < numberOFilters; index++)
-    // {
-    //     active_learning.layers[0].b.data[index] = (float)  conv3_b[index] /  powf(2, CONV3_BIAS_Q);
-    //     // printf("%f, ",active_learning.layers[0].b.data[index]);
-    // }
     
     //free the filter indexes
     while (start != NULL)
@@ -301,70 +181,35 @@ int main(void)
     }
 
 
-
-    // active_learning.layers[1] = make_batchnorm_layer(numberOFilters);
+    // define the new network
     active_learning.layers[1] = make_activation_layer(RELU);
-    // active_learning.layers[2] = make_maxpool_layer( POOL3_IM_DIM, POOL3_IM_DIM, POOL3_IM_CH,  POOL3_KER_DIM , POOL3_STRIDE); 
-    // active_learning.layers[2] = make_maxpool_layer( POOL3_IM_DIM, POOL3_IM_DIM, numberOFilters,  POOL3_KER_DIM , POOL3_STRIDE); 
-    
-    // int convout = numberOFilters*POOL3_OUT_DIM*POOL3_OUT_DIM;
     int convout = numberOFilters*CONV3_OUT_DIM*CONV3_OUT_DIM;
     active_learning.layers[2] = make_connected_layer(convout, CLASSES); 
     active_learning.layers[3] = make_activation_layer(SOFTMAX);
+    // freeze is flag to train or not
     active_learning.layers[3].freeze = 1;
-    // for (int index = 0; index < INTERFACE_WT_SHAPE; index++)
-    // for (int index = 0; index < convout*INTERFACE_OUT; index++)
-    // {
-    //     active_learning.layers[2].w.data[index] = (float) interface_w[index] /  powf(2, INTERFACE_WEIGHT_Q);
-    // }
-    
-    // for (int index = 0; index < INTERFACE_BIAS_SHAPE; index++)
-    // {
-    //     active_learning.layers[2].b.data[index] = (float)  interface_b[index] /  powf(2, INTERFACE_BIAS_Q);
-    // }
-    
-    // active_learning.layers[4] = make_activation_layer(RELU);
-    // active_learning.layers[5] = make_connected_layer(INTERFACE_OUT , CLASSES);
-    // active_learning.layers[5].freeze = 1;
 
 
-    // for (int index = 0; index < LINEAR_WT_SHAPE; index++)
-    // {
-    //     active_learning.layers[5].w.data[index] = (float) linear_w[index] /  powf(2, LINEAR_WEIGHT_Q);
-    // }
-    
-    // for (int index = 0; index < LINEAR_BIAS_SHAPE; index++)
-    // {
-    //     active_learning.layers[5].b.data[index] = (float)  linear_b[index] /  powf(2, LINEAR_BIAS_Q);
-    // }
-
-
-
-
-
-    
     int iterations = 80;
-
+    // Here we train the network
     for ( int epoch = 1; epoch < 9; epoch++){
         train_image_classifier(active_learning, training_data, batch, iterations,  rate, momentum, decay);
-        
+        // print accuracy
         float train_acc = accuracy_net(active_learning, training_data);
         printf("%d :: train acc = %f \n ",epoch, train_acc);
-        // float test_acc = accuracy_net(active_learning, test);
-        // printf("test acc = %f\n", test_acc);
-
     }
 
-
+    // on the real target you should free some of the used layers that you do not need 
     printf("\n-----------------------------------------------------------------------------\n");
+    /*
+        In this step we need to convert the network to int
+        Here is not in the most optimized way
+        Make sure you understand what we doing here before put this in your design 
+    */
 
-
-    // for (int index = 0; index < CONV3_WT_SHAPE; index++)
     for (int index = 0; index < filterSize * numberOFilters; index++)
     {
-        // active_learning.layers[0].w.data[index] = conv3_w_float [index] ;
         conv3_w_2[index] = (int) ceilf (active_learning.layers[0].w.data[index] *  powf(2, CONV3_WEIGHT_Q) );
-        // printf("%d, ",conv3_w[index]);
     }
     for (int index = 0; index < numberOFilters; index++)
     {
@@ -372,44 +217,23 @@ int main(void)
     }
 
     PoolOut = numberOFilters*CONV3_OUT_DIM*CONV3_OUT_DIM;
-    // PoolOut = CONV3_OUT_DIM* CONV3_OUT_DIM*numberOFilters;
-    // printf("2CONECTED IN %d\n" ,PoolOut );
     active_learning.n = 4;
     active_learning.layers = calloc(4, sizeof(layer));   
-
-    // active_learning.layers[0] = make_activation_layer(RELU);
     active_learning.layers[0] = make_connected_layer(PoolOut, 64);
     active_learning.layers[0].freeze =0;
-    // for (int index = 0; index < PoolOut*INTERFACE_OUT; index++)
-    // {
-    //     interface_w[index] = (int) ceilf (active_learning.layers[3].w.data[index] *  powf(2, INTERFACE_WEIGHT_Q) );
-    // }
-    
-    // for (int index = 0; index < INTERFACE_BIAS_SHAPE; index++)
-    // {
-    //     interface_b[index] = (int) ceilf (active_learning.layers[3].b.data[index] *  powf(2, INTERFACE_WEIGHT_Q) );
-    // }
-
-    // active_learning.layers[0] =  active_learning.layers[2];
-    active_learning.layers[1] =  make_activation_layer(RELU); // active_learning.layers[3];
-    // active_learning.layers[2] =  active_learning.layers[4];
-    // active_learning.layers[2] = //active_learning.layers[5]; // make_connected_layer(INTERFACE_OUT , CLASSES);
+    active_learning.layers[1] =  make_activation_layer(RELU);
     active_learning.layers[2] = make_connected_layer(64 , CLASSES);
-     active_learning.layers[2].freeze =0;
+    active_learning.layers[2].freeze =0;
+    active_learning.layers[3] = make_activation_layer(SOFTMAX); 
 
-    active_learning.layers[3] = make_activation_layer(SOFTMAX); //active_learning.layers[5];
-    // active_learning.layers[4] =  active_learning.layers[5];
-
-
-
-    data training_data2;
-    // PoolOut = POOL3_OUT_DIM* POOL3_OUT_DIM*numberOFilters;
-  
+    /*
+        The next step is for fine tuning
+        For host platform is the easy way to run experiments
+        It needs to be optimized if you plan to put on target!
+    */
+    data training_data2;  
     matrix q_X = make_matrix(NODE_0_TOTAL_TRAIN_IMAGES,PoolOut);
     matrix q_Y = make_matrix(NODE_0_TOTAL_TRAIN_IMAGES,CLASSES);
-
-    // float scale = 1 << CONV1_OUT_Q;
-
     for(int img_row=0; img_row < NODE_0_TOTAL_TRAIN_IMAGES; img_row++)
     {
         index = network2(NODE_0_TRAIN_IMAGES[img_row]);
@@ -452,6 +276,8 @@ int main(void)
 
     }
     float acc2 = accuracy_net(active_learning, test2);
+
+    // The follow is only where running experiments on host platform
     FILE* fp ;
     fp = fopen("Accx24-3classes-500-samples.csv", "a"); // save result in csv file
     fprintf(fp, "%f, \n", acc2);
@@ -466,7 +292,7 @@ int main(void)
 uint32_t network(q7_t* input)
 {
 
- //conv 1
+ //conv layer 1
     	arm_convolve_HWC_q7_basic(input, CONV1_IN_DIM, CONV1_IN_CH, conv1_w, 
                         CONV1_OUT_CH, CONV1_KER_DIM, CONV1_PADDING, CONV1_STRIDE, 
                         conv1_b, CONV1_BIAS_LSHIFT, 
@@ -475,10 +301,7 @@ uint32_t network(q7_t* input)
 
         arm_relu_q7(conv1_out, CONV1_OUT_DIM * CONV1_OUT_DIM * CONV1_OUT_CH);
 
-
-
-
-//conv 2
+//conv layer  2
         arm_convolve_HWC_q7_basic(conv1_out, CONV2_IN_DIM, CONV2_IN_CH, conv2_w, CONV2_OUT_CH, CONV2_KER_DIM,
 						  CONV2_PADDING, CONV2_STRIDE, conv2_b, CONV2_BIAS_LSHIFT, CONV2_OUT_RSHIFT, conv2_out,
 						  CONV2_OUT_DIM, conv_buffer, NULL);
@@ -486,7 +309,7 @@ uint32_t network(q7_t* input)
 
         arm_relu_q7(conv2_out, CONV2_OUT_DIM * CONV2_OUT_DIM * CONV2_OUT_CH);
 
-//conv 3
+//conv layer 3
         arm_convolve_HWC_q7_basic(conv2_out, CONV3_IN_DIM, CONV3_IN_CH, conv3_w, CONV3_OUT_CH, CONV3_KER_DIM,
 						  CONV3_PADDING, CONV3_STRIDE, conv3_b, CONV3_BIAS_LSHIFT, CONV3_OUT_RSHIFT, conv3_out,
 						  CONV3_OUT_DIM, conv_buffer, NULL);
@@ -495,16 +318,12 @@ uint32_t network(q7_t* input)
 //fully connected
  	arm_fully_connected_q7(conv3_out, fc1_w, INTERFACE_DIM, INTERFACE_OUT, INTERFACE_BIAS_LSHIFT, INTERFACE_OUT_RSHIFT, fc1_b,
 						  fc1_out, fc_buffer);
-
-
 	
     arm_softmax_q7(fc1_out, INTERFACE_OUT, y_out);
 
     uint32_t class = 0;
-    uint32_t blockSize = sizeof(y_out);
     for (int i = 0; i < 10; i++)
     {
-        // rt_kprintf(output_data[i]);
         if (y_out[i] > y_out[class])
             class = i;
     }
@@ -527,22 +346,47 @@ uint32_t network2(q7_t* input)
     arm_relu_q7(conv1_out, CONV1_OUT_DIM * CONV1_OUT_DIM * CONV1_OUT_CH);
 
 
-
     arm_convolve_HWC_q7_basic(conv1_out, CONV2_IN_DIM, CONV2_IN_CH, conv2_w, numberOFiltersConv2, CONV2_KER_DIM,
 						  CONV2_PADDING, CONV2_STRIDE, conv2_b, CONV2_BIAS_LSHIFT, CONV2_OUT_RSHIFT, conv2_out,
 						  CONV2_OUT_DIM, conv_buffer, NULL);
     arm_relu_q7(conv2_out, CONV2_OUT_DIM * CONV2_OUT_DIM * CONV2_OUT_CH);
 
 
-//////// conv 3 
     arm_convolve_HWC_q7_basic(conv2_out, CONV3_IN_DIM, CONV3_IN_CH, conv3_w_2, numberOFilters, CONV3_KER_DIM,
 						  CONV3_PADDING, CONV3_STRIDE, conv3_b_2, CONV3_BIAS_LSHIFT, CONV3_OUT_RSHIFT, conv3_out,
 						  CONV3_OUT_DIM, conv_buffer, NULL);
-// 
-//         arm_maxpool_q7_HWC(conv3_out, POOL3_IM_DIM, POOL3_IM_CH, POOL3_KER_DIM, POOL3_PADDING, POOL3_STRIDE, POOL3_OUT_DIM,  (q7_t *) conv_buffer, pool3_out);
-//         arm_relu_q7(pool3_out, POOL3_OUT_DIM * POOL3_OUT_DIM * numberOFilters); 
-// ///////
+
         
-        return 0;
+    return 0;
 }
 
+
+// helping function 
+void insert( int item, float itprio)
+{
+    // int item, itprio; 
+    new = (N *)malloc(sizeof(N));
+
+    new->info = item;
+    new->priority = itprio;
+    new->next = NULL;
+    if (start == NULL)
+    {
+        start = new;
+    }
+    else if (start != NULL && itprio < start->priority)
+    {
+        new->next = start;
+        start = new;
+    }
+    else
+    {
+        q = start;
+        while (q->next != NULL && q->next->priority < itprio)
+        {
+            q = q->next;
+        }
+        new->next = q->next;
+        q->next = new;
+    }
+}
